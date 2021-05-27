cd1365ac4f1ee5fab7fcc1733df661e574c59c51
Chae-EunJeong <chaeun.jjj@gmail.com>
2021-05-27 17:56:20 +0900

    react practicing. making news search

---
+++ newssearching/src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import AxiosComponent from "./component/AxiosComponent";
+import AuthResultPage from "./pages/AuthResultPage";
+import MainPage from "./pages/MainPage";
+import NewsPage from "./pages/NewsPage";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/axios" exact>
+          <AxiosComponent />
+        </Route>
+        <Route path="/news" exact>
+          <NewsPage />
+        </Route>
+        <Route path="/main" exact>
+          <MainPage />
+        </Route>
+        <Route path="/authResult" exact>
+          <AuthResultPage />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
